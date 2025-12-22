c5756d3afaf018d8ef3b07f349f693225df461f2
gkoduol <gkoduol@terpmail.umd.edu>
2025-12-21 21:59:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,14 @@ const PORT = 3001;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/Jobs.html"));
-  });
+const publicPath = path.join(__dirname, 'public');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'Jobs.html'));
+});
   
 
 app.get("/api/test", (req, res) => {
